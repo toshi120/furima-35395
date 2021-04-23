8734bc62ce9444054878a450fedbaffa3d5b1b83
toshi120 <toshizy@gmail.com>
2021-04-23 19:09:32 +0900

    新規登録機能
    新規登録ページの他に
    ログインの有無で表示変更
    ログアウトボタン
    ログインボタンも実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
+  resources :user, only: [:new, :create, :destroy]
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,9 @@
 ActiveRecord::Schema.define(version: 2021_04_23_020909) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "nickname", null: false
     t.string "family_name", null: false
     t.string "first_name", null: false
     t.string "family_name_kana", null: false
