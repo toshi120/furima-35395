e5d5ac27ca92886d109f744c80a979373e5aafc1
toshi120 <toshizy@gmail.com>
2021-04-26 12:45:22 +0900

    RSpecとfakerの導入

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'ユーザー新規登録' do
+    context '新規登録できるとき' do
+      it '全ての項目が存在すれば登録できる' do
+      end
+      
+      it ''
+    end
+  end
+end
