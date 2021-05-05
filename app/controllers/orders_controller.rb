7256a6d9b4c98ff638d64af2b94a49c6dc38bc98
toshi120 <toshizy@gmail.com>
2021-05-05 17:10:31 +0900

    order controllerにbefore actionの追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,7 @@
 class OrdersController < ApplicationController
-before_action :set_item, only: [:index, :create]
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
+  before_action :redirect_root_path, only: [:index, :create]
 
   def index
     @order_address = OrderAddress.new
@@ -24,6 +26,12 @@ before_action :set_item, only: [:index, :create]
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def redirect_root_path
+    if current_user.id == @item.user_id || Order.exists?(item_id: @item.id)
+      redirect_to root_path
+    end
   end
 
   def pay_item
